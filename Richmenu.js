68bb9f19e62bcd0ebb7bac7761910627917c923c
yoobaring <yoo.2538baring88@gmail.com>
2023-01-08 10:11:42 +0700

    first commit

---
+++ Richmenu.js
@@ -0,0 +1,155 @@
+{
+      "size": {
+        "width": 2500,
+        "height": 1686
+      },
+      "selected": true,
+      "name": "ริชเมนู 3",
+      "chatBarText": "ประกาศ",
+      "areas": [
+        {
+          "bounds": {
+            "x": 3,
+            "y": 4,
+            "width": 624,
+            "height": 554
+          },
+          "action": {
+            "type": "message",
+            "text": "text message"
+          }
+        },
+        {
+          "bounds": {
+            "x": 2,
+            "y": 561,
+            "width": 624,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "quick reply "
+          }
+        },
+        {
+          "bounds": {
+            "x": 626,
+            "y": 557,
+            "width": 620,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "sticker message"
+          }
+        },
+        {
+          "bounds": {
+            "x": 1248,
+            "y": 563,
+            "width": 630,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "file message"
+          }
+        },
+        {
+          "bounds": {
+            "x": 1880,
+            "y": 566,
+            "width": 620,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "location message"
+          }
+        },
+        {
+          "bounds": {
+            "x": 1876,
+            "y": 0,
+            "width": 620,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "video message"
+          }
+        },
+        {
+          "bounds": {
+            "x": 1247,
+            "y": 0,
+            "width": 620,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "audio message"
+          }
+        },
+        {
+          "bounds": {
+            "x": 626,
+            "y": 0,
+            "width": 620,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "image  message"
+          }
+        },
+        {
+          "bounds": {
+            "x": 0,
+            "y": 1121,
+            "width": 620,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "image carousel"
+          }
+        },
+        {
+          "bounds": {
+            "x": 616,
+            "y": 1118,
+            "width": 647,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "bubble"
+          }
+        },
+        {
+          "bounds": {
+            "x": 1246,
+            "y": 1121,
+            "width": 637,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "card carousel "
+          }
+        },
+        {
+          "bounds": {
+            "x": 1880,
+            "y": 1121,
+            "width": 620,
+            "height": 565
+          },
+          "action": {
+            "type": "message",
+            "text": "confirm card"
+          }
+        }
+      ]
+}
